13b818b62a9cc474959d84ab47f5793876c12dda
RaynaldoAdp <raynaldoadp@gmail.com>
2016-10-04 13:26:43 +0700

    text-analyzer query

---
+++ app.js
@@ -0,0 +1,53 @@
+function getInput(){
+	$('.js-form').submit(function(event){
+		event.preventDefault();
+		var textInput = $('#user-text').val();
+		alert(textInput);
+	});
+}
+getInput();
+
+function getWords(wordsString) {
+ 	return wordsString.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
+}
+
+function getCharacters(wordsString){
+	return wordsString.toLowerCase().split(" ").filter(Boolean).sort();
+}
+
+function wordCount(wordsArray){
+	return words.length;
+}
+
+function arrayToObject(wordsArray){
+	var object = {};
+	for(i = 0; i < wordsArray.length; i++){
+		if([wordsArray[i]] in object){
+			object[wordsArray[i]]++; 
+		}
+		else{
+			object[wordsArray[i]] = 1;
+		}
+	}
+	return object;
+}
+
+function averageWordLength(wordsArray){
+	var totalLength = 0;
+	for(i = 0; i < wordsArray.length; i++){
+		totalLength = totalLength + wordsArray[i].length;
+	}
+	return totalLength / wordsArray.length;
+}
+
+function UniqueWordCount(wordsObject){
+	return Object.keys(wordsObject).length;
+}
+
+
+
+
+
+
+	
+
